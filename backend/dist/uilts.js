9cff6433fb24c83730b9db3e2fb8a7e5fd1f748b
ShayTSC <zhang951005@gmail.com>
2022-12-11 09:22:09 +0800

    add dist folder to .gitignore

--- backend/dist/uilts.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sleep = exports.illegalCoordinate = exports.illegalNumber = void 0;
-const illegalNumber = (value) => isNaN(value) || value < 1 || value > 200;
-exports.illegalNumber = illegalNumber;
-const illegalCoordinate = (coord) => coord.x < 0 || coord.x > 2000 || coord.y < 0 || coord.y > 2000;
-exports.illegalCoordinate = illegalCoordinate;
-const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-exports.sleep = sleep;
